38aafb1a45b8912a18403424ef9357523794f4d4
n_apier <69126935+n-apier@users.noreply.github.com>
2021-08-07 12:07:19 -0700

    Add files via upload

---
+++ Subject_Projects/bastafazoolin.py
@@ -0,0 +1,71 @@
+class Menu:
+  def __init__(self, name, items, start_time, end_time):
+    self.name = name
+    self.items =  items
+    self.start_time = start_time
+    self.end_time = end_time
+
+  def __repr__(self):
+    return "{name} menu available from {start_time} to {end_time}".format(name = self.name, start_time = self.start_time, end_time = self.end_time)
+  
+  def calculate_bill(self, purchased_items):
+    bill = 0
+    self.purchased_items = purchased_items
+    for item in purchased_items:
+      for key, value in self.items.items():
+        if item == key:
+         bill += value
+    return bill
+
+
+brunch = Menu("brunch", {'pancakes': 7.50, 'waffles': 9.00, 'burger': 11.00, 'home fries': 4.50, 'coffee': 1.50, 'espresso': 3.00, 'tea': 1.00, 'mimosa': 10.50, 'orange juice': 3.50
+}, 11, 16)
+
+early_bird = Menu("early_bird", {'salumeria plate': 8.00, 'salad and breadsticks (serves 2, no refills)': 14.00, 'pizza with quattro formaggi': 9.00, 'duck ragu': 17.50, 'mushroom ravioli (vegan)': 13.50, 'coffee': 1.50, 'espresso': 3.00,
+}, 15, 18)
+
+dinner = Menu("dinner", {'crostini with eggplant caponata': 13.00, 'ceaser salad': 16.00, 'pizza with quattro formaggi': 11.00, 'duck ragu': 19.50, 'mushroom ravioli (vegan)': 13.50, 'coffee': 2.00, 'espresso': 3.00,
+}, 17, 23)
+
+kids = Menu("kids", {'chicken nuggets': 6.50, 'fusilli with wild mushrooms': 12.00, 'apple juice': 3.00
+}, 11, 21)
+
+
+
+class Franchise:
+  def __init__(self, address, menus):
+    self.address = address
+    self.menus = menus
+
+  def __repr__(self):
+    return self.address
+  
+  def available_menus(self, time):
+    self.time = time
+    menulist = []
+    for menu in self.menus:
+      if time >= menu.start_time and time <= menu.end_time:
+        for key in menu.items:
+          menulist.append(key)
+    return menulist
+
+class Business:
+  def __init__(self, name, franchises):
+    self.name = name
+    self.franchises = franchises
+
+arepas_menu = {
+  'arepa pabellon': 7.00, 'pernil arepa': 8.50, 'guayanes arepa': 8.00, 'jamon arepa': 7.50
+}
+
+arepas_place = Franchise("189 Fitzgerald Avenue", arepas_menu)
+flagship_store = Franchise("1232 West End Road", [brunch, early_bird, dinner, kids])
+new_installment = Franchise("12 East Mulberry Street", [brunch, early_bird, dinner, kids])
+
+business = Business("Basta Fazoolin' with my Heart", [flagship_store, new_installment])
+
+arepa = Business("Take a' Arepa", [arepas_place])
+
+print(flagship_store.available_menus(17))
+print(flagship_store)
+print(brunch.calculate_bill(['pancakes', 'home fries', 'coffee']))
